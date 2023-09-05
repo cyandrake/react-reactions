bc4aebde024023fb2849b9b6e36294a22db5f5cb
iaN <36753455+cyandrake@users.noreply.github.com>
2023-09-05 01:25:20 +0000

    add reaction component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import Reactions from './Reactions';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>React Reactions</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div className="" style={{display:'flex', justifyContent:'center'}}>
+        <Reactions/>
+      </div>
     </div>
   );
 }
